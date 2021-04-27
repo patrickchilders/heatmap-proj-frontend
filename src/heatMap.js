84157c4d98f4ad74169dfd1b6fe104bf4787afa6
patrickchilders <patrickchilders11@gmail.com>
2021-04-27 13:34:02 -0400

    added heroku procfile, removed hanging dependency

--- src/heatMap.js
+++ src/heatMap.js
@@ -8,12 +8,28 @@ import 'leaflet.heat';
 function Heatmap(props){
    const map = useMap()
    useEffect(() => {
-     const points = props.addressPoints
-     ? props.addressPoints.map((p) => {
-       return [p[0], p[1], p[2]]; // lat lng intensity
-       })
-     : [];
-     L.heatLayer(points).addTo(map);
+    //  let latlongBounds = map.getBounds();
+    //  let apiBounds = [
+    //    [latlongBounds.getNorthWest().lat, latlongBounds.getNorthWest().long],
+    //    [latlongBounds.getNorthEast().lat, latlongBounds.getNorthEast().long],
+    //    [latlongBounds.getSouthWest().lat, latlongBounds.getSouthWest().long],
+    //    [latlongBounds.getSouthEast().lat, latlongBounds.getSouthEast().long]
+    //  ];
+    let points = [];
+    fetch('https://heatmap-proj-backend.herokuapp.com/geoip')
+      .then(res => res.json())
+      .then((data) => {
+        points = data.map((point) => {
+          return [parseFloat(point[0]), parseFloat(point[1])];
+        });
+     })
+
+    //  const points = props.addressPoints
+    //  ? props.addressPoints.map((p) => {
+    //    return [p[0], p[1], p[2]]; // lat lng intensity
+    //    })
+    //  : [];
+    L.heatLayer(points).addTo(map);
    }, []);
    return null;
 }
--- src/reducer.js
+++ src/reducer.js
@@ -1,5 +1,3 @@
-import { combineReducers } from 'redux'
-
 const initialState = {
     bounds: [
         [53,-46,0.5],
